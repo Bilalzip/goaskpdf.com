a3923c575a1cf8fb2e01d1df8c55f4089fc9faea
bilalzip <70857108+Bilalzip@users.noreply.github.com>
2024-02-26 13:33:34 +0530

    new

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
    
-      <SignUp redirectUrl={'/'} />
+      <SignUp redirectUrl={'https://www.goaskpdf.com/'} />
 
   );
 }
